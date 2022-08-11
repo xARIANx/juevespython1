2f651c3706f2307067f85dab7dd7ec167cda116c
masthe <102667166+masthe@users.noreply.github.com>
2022-08-11 08:48:47 -0500

    Se hace foto de la entrada por teclado en python

---
+++ main.py
@@ -0,0 +1,16 @@
+#1. DATOS PRIMITIVOS
+nombre = "Arian"
+edad = 33
+estatura = 1.62
+hinchaDelMejor = True
+
+#2. DATOS QUE ENTRAN POR EL TECLADO
+
+
+numero1 = int(input("Digite un numero: "))
+numero2 = int(input("Digite el numero 2: "))
+
+suma = numero1 + numero2
+print(f'La suma de {numero1} y {numero2} es {suma}')
+
+
